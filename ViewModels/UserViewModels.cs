c4dfe31b0f05fc041639d6c3110497d655b62080
djerass <dsaraula@yandex.ru>
2018-09-16 01:23:04 +0300

    1. Add controller for usercontrol 2. Add Delete/Edit/Create/Look all Users

---
+++ ViewModels/UserViewModels.cs
@@ -0,0 +1,15 @@
+namespace ToDo_Diary.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Year { get; set; }
+    }
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+    }
+}
